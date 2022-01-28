bae0e577e5f5a44af63b0948422c27e5bb211d3a
Matthew Ballard <matthewrobertballard@gmail.com>
2022-01-27 19:36:37 -0500

    init init

---
+++ init.lua
@@ -0,0 +1,459 @@
+-- Install packe
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+vim.cmd [[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]]
+
+local use = require('packer').use
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'tpope/vim-fugitive' -- Git commands in nvim
+  use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
+  -- "gc" to comment visual regions/lines
+  use { 'numToStr/Comment.nvim',
+	config = function()
+          require('Comment').setup()
+    	end
+  }
+  use 'ludovicchabant/vim-gutentags' -- Automatic tags management
+  -- UI to select things (files, grep results, open buffers...)
+  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use 'mjlbach/onedark.nvim' -- Theme inspired by Atom
+  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+  -- Add indentation guides even on blank lines
+  use 'lukas-reineke/indent-blankline.nvim'
+  -- Add git related info in the signs columns and popups
+  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+	-- Manage PRs in vim
+  use { 'pwntester/octo.nvim',
+  requires = {'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim', 'kyazdani42/nvim-web-devicons'},
+  config = function ()
+	  require"octo".setup()
+  end
+  }
+  -- Highlight, edit, and navigate code using a fast incremental parsing library
+  use 'nvim-treesitter/nvim-treesitter'
+  -- Additional textobjects for treesitter
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'lervag/vimtex' -- Latex assist
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'rafamadriz/friendly-snippets' -- VS Code like snippets
+end)
+
+--Set highlight on search
+vim.o.hlsearch = false
+
+--Make line numbers default
+vim.wo.number = true
+
+--Enable mouse mode
+vim.o.mouse = 'a'
+
+--Enable break indent
+vim.o.breakindent = true
+
+--Save undo history
+vim.opt.undofile = true
+
+--Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+
+--Set colorscheme
+vim.o.termguicolors = true
+vim.cmd [[colorscheme onedark]]
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+--Set statusbar
+require('lualine').setup {
+  options = {
+    icons_enabled = false,
+    theme = 'onedark',
+    component_separators = '|',
+    section_separators = '',
+  },
+}
+
+--Load snippets
+require("luasnip.loaders.from_vscode").load()
+
+--Enable Comment.nvim
+require('Comment').setup()
+
+--Remap space as leader key
+vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+--Remap for dealing with word wrap
+vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+
+
+-- Highlight on yank
+vim.cmd [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]]
+
+--Map blankline
+vim.g.indent_blankline_char = '┊'
+vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
+vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+
+-- Gitsigns
+require('gitsigns').setup {
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+}
+
+-- Telescope
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+}
+
+-- Enable telescope fzf native
+require('telescope').load_extension 'fzf'
+
+--Add leader shortcuts
+vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
+
+-- Treesitter configuration
+-- Parsers must be installed manually via :TSInstall
+require('nvim-treesitter.configs').setup {
+  highlight = {
+    enable = true, -- false will disable the whole extension
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
+  },
+  indent = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+  },
+}
+
+-- Diagnostic keymaps
+vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
+
+-- LSP settings
+local lspconfig = require 'lspconfig'
+local on_attach = function(_, bufnr)
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+end
+
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- Enable the following language servers
+local servers = { 'clangd', 'gopls', 'rust_analyzer', 'pyright', 'tsserver'}
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- Example custom server
+-- Make runtime files discoverable to the server
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-c>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
+
+require"octo".setup({
+  default_remote = {"upstream", "origin"}; -- order to try remotes
+  reaction_viewer_hint_icon = "";         -- marker for user reactions
+  user_icon = " ";                        -- user icon
+  timeline_marker = "";                   -- timeline marker
+  timeline_indent = "2";                   -- timeline indentation
+  right_bubble_delimiter = "";            -- Bubble delimiter
+  left_bubble_delimiter = "";             -- Bubble delimiter
+  github_hostname = "";                    -- GitHub Enterprise host
+  snippet_context_lines = 4;               -- number or lines around commented lines
+  file_panel = {
+    size = 10,                             -- changed files panel rows
+    use_icons = true                       -- use web-devicons in file panel
+  },
+  mappings = {
+    issue = {
+      close_issue = "<space>ic",           -- close issue
+      reopen_issue = "<space>io",          -- reopen issue
+      list_issues = "<space>il",           -- list open issues on same repo
+      reload = "<C-r>",                    -- reload issue
+      open_in_browser = "<C-b>",           -- open issue in browser
+      copy_url = "<C-y>",                  -- copy url to system clipboard
+      add_assignee = "<space>aa",          -- add assignee
+      remove_assignee = "<space>ad",       -- remove assignee
+      create_label = "<space>lc",          -- create label
+      add_label = "<space>la",             -- add label
+      remove_label = "<space>ld",          -- remove label
+      goto_issue = "<space>gi",            -- navigate to a local repo issue
+      add_comment = "<space>ca",           -- add comment
+      delete_comment = "<space>cd",        -- delete comment
+      next_comment = "]c",                 -- go to next comment
+      prev_comment = "[c",                 -- go to previous comment
+      react_hooray = "<space>rp",          -- add/remove 🎉 reaction
+      react_heart = "<space>rh",           -- add/remove ❤️ reaction
+      react_eyes = "<space>re",            -- add/remove 👀 reaction
+      react_thumbs_up = "<space>r+",       -- add/remove 👍 reaction
+      react_thumbs_down = "<space>r-",     -- add/remove 👎 reaction
+      react_rocket = "<space>rr",          -- add/remove 🚀 reaction
+      react_laugh = "<space>rl",           -- add/remove 😄 reaction
+      react_confused = "<space>rc",        -- add/remove 😕 reaction
+    },
+    pull_request = {
+      checkout_pr = "<space>po",           -- checkout PR
+      merge_pr = "<space>pm",              -- merge PR
+      list_commits = "<space>pc",          -- list PR commits
+      list_changed_files = "<space>pf",    -- list PR changed files
+      show_pr_diff = "<space>pd",          -- show PR diff
+      add_reviewer = "<space>va",          -- add reviewer
+      remove_reviewer = "<space>vd",       -- remove reviewer request
+      close_issue = "<space>ic",           -- close PR
+      reopen_issue = "<space>io",          -- reopen PR
+      list_issues = "<space>il",           -- list open issues on same repo
+      reload = "<C-r>",                    -- reload PR
+      open_in_browser = "<C-b>",           -- open PR in browser
+      copy_url = "<C-y>",                  -- copy url to system clipboard
+      add_assignee = "<space>aa",          -- add assignee
+      remove_assignee = "<space>ad",       -- remove assignee
+      create_label = "<space>lc",          -- create label
+      add_label = "<space>la",             -- add label
+      remove_label = "<space>ld",          -- remove label
+      goto_issue = "<space>gi",            -- navigate to a local repo issue
+      add_comment = "<space>ca",           -- add comment
+      delete_comment = "<space>cd",        -- delete comment
+      next_comment = "]c",                 -- go to next comment
+      prev_comment = "[c",                 -- go to previous comment
+      react_hooray = "<space>rp",          -- add/remove 🎉 reaction
+      react_heart = "<space>rh",           -- add/remove ❤️ reaction
+      react_eyes = "<space>re",            -- add/remove 👀 reaction
+      react_thumbs_up = "<space>r+",       -- add/remove 👍 reaction
+      react_thumbs_down = "<space>r-",     -- add/remove 👎 reaction
+      react_rocket = "<space>rr",          -- add/remove 🚀 reaction
+      react_laugh = "<space>rl",           -- add/remove 😄 reaction
+      react_confused = "<space>rc",        -- add/remove 😕 reaction
+    },
+    review_thread = {
+      goto_issue = "<space>gi",            -- navigate to a local repo issue
+      add_comment = "<space>ca",           -- add comment
+      add_suggestion = "<space>sa",        -- add suggestion
+      delete_comment = "<space>cd",        -- delete comment
+      next_comment = "]c",                 -- go to next comment
+      prev_comment = "[c",                 -- go to previous comment
+      select_next_entry = "]q",            -- move to previous changed file
+      select_prev_entry = "[q",            -- move to next changed file
+      close_review_tab = "<C-c>",          -- close review tab
+      react_hooray = "<space>rp",          -- add/remove 🎉 reaction
+      react_heart = "<space>rh",           -- add/remove ❤️ reaction
+      react_eyes = "<space>re",            -- add/remove 👀 reaction
+      react_thumbs_up = "<space>r+",       -- add/remove 👍 reaction
+      react_thumbs_down = "<space>r-",     -- add/remove 👎 reaction
+      react_rocket = "<space>rr",          -- add/remove 🚀 reaction
+      react_laugh = "<space>rl",           -- add/remove 😄 reaction
+      react_confused = "<space>rc",        -- add/remove 😕 reaction
+    },
+    submit_win = {
+      approve_review = "<C-a>",            -- approve review
+      comment_review = "<C-m>",            -- comment review
+      request_changes = "<C-r>",           -- request changes review
+      close_review_tab = "<C-c>",          -- close review tab
+    },
+    review_diff = {
+      add_review_comment = "<space>ca",    -- add a new review comment
+      add_review_suggestion = "<space>sa", -- add a new review suggestion
+      focus_files = "<leader>e",           -- move focus to changed file panel
+      toggle_files = "<leader>b",          -- hide/show changed files panel
+      next_thread = "]t",                  -- move to next thread
+      prev_thread = "[t",                  -- move to previous thread
+      select_next_entry = "]q",            -- move to previous changed file
+      select_prev_entry = "[q",            -- move to next changed file
+      close_review_tab = "<C-c>",          -- close review tab
+      toggle_viewed = "<leader><space>",   -- toggle viewer viewed state
+    },
+    file_panel = {
+      next_entry = "j",                    -- move to next changed file
+      prev_entry = "k",                    -- move to previous changed file
+      select_entry = "<cr>",               -- show selected changed file diffs
+      refresh_files = "R",                 -- refresh changed files panel
+      focus_files = "<leader>e",           -- move focus to changed file panel
+      toggle_files = "<leader>b",          -- hide/show changed files panel
+      select_next_entry = "]q",            -- move to previous changed file
+      select_prev_entry = "[q",            -- move to next changed file
+      close_review_tab = "<C-c>",          -- close review tab
+      toggle_viewed = "<leader><space>",   -- toggle viewer viewed state
+    }
+  }
+})
